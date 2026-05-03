f3ddf1a43818c849cd250f5f165b517534b17bd2
Sunpreet Singh <168051761+SunSinD@users.noreply.github.com>
2026-05-02 23:53:14 -0400

    Fix boot defaults for host overrides

--- modules/nixos/core.nix
+++ modules/nixos/core.nix
@@ -90,8 +90,8 @@
       GTK_THEME                   = "Adwaita:dark";
     };
 
-    boot.loader.systemd-boot.configurationLimit = 2;
-    boot.loader.timeout = 0;
+    boot.loader.systemd-boot.configurationLimit = lib.mkDefault 2;
+    boot.loader.timeout = lib.mkDefault 0;
     boot.consoleLogLevel = 0;
     boot.kernelParams = [ "quiet" "udev.log_level=3" ];
 
