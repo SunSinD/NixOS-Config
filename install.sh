00de766633f1a87a91970504e19b4d9a7b073b83
SunSinD <sunpreetsingh22@outlook.com>
2026-05-04 23:24:33 -0400

    Use target Nix store for flake lock

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ INSTALL_SUBSTITUTERS="https://cache.nixos.org https://niri.cachix.org https://at
 INSTALL_TRUSTED_KEYS="cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964= lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc= cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
 NIX_FLAGS=(
   --extra-experimental-features "nix-command flakes"
+  --store /mnt
+  --eval-store /mnt
   --option substituters "$INSTALL_SUBSTITUTERS"
   --option trusted-public-keys "$INSTALL_TRUSTED_KEYS"
 )
